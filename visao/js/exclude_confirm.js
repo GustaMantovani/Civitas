5c7927f7427d1f46a377dff6bf196f2c639abb60
Gusta <112289861+GustaMantovani@users.noreply.github.com>
2023-11-12 07:31:19 -0300

    Update exclude_confirm.js

--- visao/js/exclude_confirm.js
+++ visao/js/exclude_confirm.js
@@ -1,6 +1,5 @@
 function confirmarExclusao(idUsuario) {
   var resposta = confirm("Tem certeza que deseja excluir este usuário?");
-
   if (resposta) {
     window.location.href = "../controlador/excluirUsr.php?id=" + idUsuario;
   } else {
